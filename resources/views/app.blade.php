cba9f3672a765fcc23625ef4dec3e4b1f4d844e2
castiyo0429 <castiyo0429@gmail.com>
2025-05-17 00:40:26 -0500

    Update app title and description in head section

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,24 +1,19 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <title inertia>OnLaBox - Simula, Aprende y Practica</title>
+    <meta name="description" content="Simulación interactiva para aprender con ejemplos reales del SAT.">
+    Aprende Laravel Interactivamente
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('storage/img/icon.svg') }}" type="image/svg+xml">
 
-         <!-- Favicon -->
-        <link rel="icon" href="{{ asset('storage/img/icon.svg') }}" type="image/svg+xml">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @routes
-        @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
-</html>
+    <!-- Scripts -->
+    @routes
+    @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
+    @inertiaHead
+</head>
